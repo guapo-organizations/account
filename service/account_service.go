a745b828765e5eae63040c6ff9892daf8bcf1e7d
liangyu <liangyu@22.cn>
2019-05-09 19:00:55 +0800

    验证文件放到src里面去

--- service/account_service.go
+++ service/account_service.go
@@ -3,8 +3,8 @@ package service
 import (
 	"context"
 	"fmt"
-	"regexp"
 	"zldz/account-service/proto/account"
+	"zldz/go-micro-secret/help"
 )
 
 type AccountService struct {
@@ -15,29 +15,15 @@ func (this *AccountService) RegisterAccountByEmailOrPhone(ctx context.Context, i
 	//响应
 	response := new(account.UserBaseInfo)
 
-	if this.VerifyEmailFormat(in.Email) {
+	if help.VerifyEmailFormat(in.Email) {
 		//邮箱注册
-	} else if this.VerifyMobileFormat(in.Phone) {
+	} else if help.VerifyMobileFormat(in.Phone) {
 		//手机号注册
 	} else {
 		return nil, fmt.Errorf("手机号和邮箱必填一个")
 	}
 
 	response.Account = "测试账号"
-	
+
 	return response, nil
-}
-
-//验证是否为邮箱
-func (this *AccountService) VerifyEmailFormat(email string) bool {
-	pattern := `^[0-9a-z][_.0-9a-z-]{0,31}@([0-9a-z][0-9a-z-]{0,30}[0-9a-z]\.){1,4}[a-z]{2,4}$`
-	reg := regexp.MustCompile(pattern)
-	return reg.MatchString(email)
-}
-
-//验证是否为手机号
-func (this *AccountService) VerifyMobileFormat(mobileNum string) bool {
-	regular := "^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199|(147))\\d{8}$"
-	reg := regexp.MustCompile(regular)
-	return reg.MatchString(mobileNum)
 }
