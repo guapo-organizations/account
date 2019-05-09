03089d912da91118f7fe992125084c5dc1d0ade2
liangyu <liangyu@22.cn>
2019-05-09 21:37:58 +0800

    服务

--- main.go
+++ main.go
@@ -1,34 +1,34 @@
 package main
 
 import (
+	"fmt"
+	"github.com/guapo-organizations/go-micro-secret/frame_tool"
 	"google.golang.org/grpc"
 	"log"
 	"net"
 	pb "zldz/account-service/proto/account"
-	"zldz/account-service/service"
-	_ "github.com/go-sql-driver/mysql"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
-)
-
-const (
-	port = ":50051"
+	myservice "zldz/account-service/service"
 )
 
 func main() {
 
+	//加载一些公共的组件，比如数据库之类的
+	my_frame_tool := new(frame_tool.LyFrameTool)
+	my_frame_tool.ConfigPath = "./config"
+	port := my_frame_tool.Run()
+
 	//开始监听
-	lis, err := net.Listen("tcp", port)
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", port))
 	if err != nil {
 		log.Fatalf("account服务监听失败: %v", err)
 	}
 
 	//构建服务
 	s := grpc.NewServer()
-
 	//注册服务
-	pb.RegisterAccountServer(s, &service.AccountService{})
+	pb.RegisterAccountServer(s, &myservice.AccountService{})
 
 	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+		log.Fatalf("服务出现异常: %v", err)
 	}
 }
