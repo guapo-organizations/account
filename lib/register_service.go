51cce6bdc8937a7e5f809bd225203cb9827ebaeb
liangyu <liangyu@22.cn>
2019-05-10 16:33:50 +0800

    update

--- lib/register_service.go
+++ lib/register_service.go
@@ -11,17 +11,18 @@ type RegisterService struct {
 }
 
 //邮箱注册
-func (this RegisterService) RegisterAccountByEmail(email string) (account_model *model.Account, err error) {
+func (this RegisterService) RegisterAccountByEmail(email string) (*model.Account, error) {
 
 	if !help.VerifyEmailFormat(email) {
 		return nil, fmt.Errorf("email格式不对")
 	}
 
 	db := database.GetMysqlDB()
+	account_model := new(model.Account)
 
 	db.Where(&model.Account{
 		Email: email,
-	}).Find(account_model)
+	}).First(account_model)
 
 	//找到直接返回
 	if account_model.Id != 0 {
@@ -35,16 +36,17 @@ func (this RegisterService) RegisterAccountByEmail(email string) (account_model 
 }
 
 //手机号注册
-func (this RegisterService) RegisterAccountByPhone(phone string) (account_model *model.Account, err error) {
+func (this RegisterService) RegisterAccountByPhone(phone string) (*model.Account, error) {
 
 	if !help.VerifyMobileFormat(phone) {
 		return nil, fmt.Errorf("手机号格式不对")
 	}
 	db := database.GetMysqlDB()
+	account_model := new(model.Account)
 
 	db.Where(&model.Account{
 		Phone: phone,
-	}).Find(account_model)
+	}).First(account_model)
 
 	//找到直接返回
 	if account_model.Id != 0 {
--- model/account.go
+++ model/account.go
@@ -4,11 +4,11 @@ import "github.com/jinzhu/gorm"
 
 type Account struct {
 	gorm.Model
-	Id      int64  `gorm:"PRIMARY_KEY;AUTO_INCREMENT;column:id"`
-	Passwd  string `gorm:"column:passwd"`
-	Email   string `gorm:"column:email"`
-	Phone   string `gorm:"column:phone"`
-	Name    string `gorm:"column:name"`
+	Id     int64  `gorm:"PRIMARY_KEY;AUTO_INCREMENT;column:id"`
+	Passwd string `gorm:"column:passwd"`
+	Email  string `gorm:"column:email"`
+	Phone  string `gorm:"column:phone"`
+	Name   string `gorm:"column:name"`
 }
 
 //表的名字
