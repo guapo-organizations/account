8ef31bc7f12f5c926d8456667cff91477e86a267
liangyu <liangyu@22.cn>
2019-05-10 19:15:12 +0800

    update

--- client/main.go
+++ client/main.go
@@ -13,50 +13,21 @@ const (
 )
 
 func main() {
-	mychan := make(chan bool)
-	go func() {
-		// Set up a connection to the server.
-		conn, err := grpc.Dial(address, grpc.WithInsecure())
-		if err != nil {
-			log.Fatalf("did not connect: %v", err)
-		}
-		defer conn.Close()
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
 
-		c := pb.NewAccountClient(conn)
+	c := pb.NewAccountClient(conn)
 
-		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-		defer cancel()
-		for i := 0; i < 1000000; i++ {
-			r, err := c.RegisterAccountByEmailOrPhone(ctx, &pb.EmailOrPhoneRequest{Phone: "13647897503"})
-			if err != nil {
-				log.Printf("could not greet: %v", err)
-			}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.RegisterAccountByEmailOrPhone(ctx, &pb.EmailOrPhoneRequest{Phone: "13647897503"})
+	if err != nil {
+		log.Printf("could not greet: %v", err)
+	}
 
-			log.Printf("结果:%v", r)
-		}
-	}()
-
-	go func() {
-		// Set up a connection to the server.
-		conn, err := grpc.Dial(address, grpc.WithInsecure())
-		if err != nil {
-			log.Fatalf("did not connect: %v", err)
-		}
-		defer conn.Close()
-
-		c := pb.NewAccountClient(conn)
-
-		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-		defer cancel()
-		for i := 0; i < 1000000; i++ {
-			r, err := c.RegisterAccountByEmailOrPhone(ctx, &pb.EmailOrPhoneRequest{Phone: "13647897504"})
-			if err != nil {
-				log.Printf("could not greet: %v", err)
-			}
-
-			log.Printf("结果:%v", r)
-		}
-	}()
-
-	<-mychan
+	log.Printf("结果:%v", r)
 }
