277098087f3fc84c64f179c57957eb86afd129d2
liangyu <liangyu@22.cn>
2019-05-10 20:43:49 +0800

    空rpc服务参数

--- proto/account/account.pb.go
+++ proto/account/account.pb.go
@@ -7,6 +7,7 @@ import (
 	context "context"
 	fmt "fmt"
 	proto "github.com/golang/protobuf/proto"
+	empty "github.com/golang/protobuf/ptypes/empty"
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
@@ -199,7 +200,7 @@ func (m *UserInfo) GetUserPlatformInfo() []*UserPlatformInfo {
 	return nil
 }
 
-//邮件或手机号请求
+//邮件或手机号注册请求
 type EmailOrPhoneRequest struct {
 	Email                string   `protobuf:"bytes,1,opt,name=email,proto3" json:"email,omitempty"`
 	Phone                string   `protobuf:"bytes,2,opt,name=phone,proto3" json:"phone,omitempty"`
@@ -247,37 +248,183 @@ func (m *EmailOrPhoneRequest) GetPhone() string {
 	return ""
 }
 
+//三方类型定义
+type Type struct {
+	Type                 int64    `protobuf:"varint,1,opt,name=type,proto3" json:"type,omitempty"`
+	Name                 string   `protobuf:"bytes,2,opt,name=name,proto3" json:"name,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *Type) Reset()         { *m = Type{} }
+func (m *Type) String() string { return proto.CompactTextString(m) }
+func (*Type) ProtoMessage()    {}
+func (*Type) Descriptor() ([]byte, []int) {
+	return fileDescriptor_5d492a0187472a3b, []int{4}
+}
+
+func (m *Type) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_Type.Unmarshal(m, b)
+}
+func (m *Type) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_Type.Marshal(b, m, deterministic)
+}
+func (m *Type) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_Type.Merge(m, src)
+}
+func (m *Type) XXX_Size() int {
+	return xxx_messageInfo_Type.Size(m)
+}
+func (m *Type) XXX_DiscardUnknown() {
+	xxx_messageInfo_Type.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_Type proto.InternalMessageInfo
+
+func (m *Type) GetType() int64 {
+	if m != nil {
+		return m.Type
+	}
+	return 0
+}
+
+func (m *Type) GetName() string {
+	if m != nil {
+		return m.Name
+	}
+	return ""
+}
+
+//三方类型
+type PlatformTypeList struct {
+	Type                 []*Type  `protobuf:"bytes,1,rep,name=type,proto3" json:"type,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *PlatformTypeList) Reset()         { *m = PlatformTypeList{} }
+func (m *PlatformTypeList) String() string { return proto.CompactTextString(m) }
+func (*PlatformTypeList) ProtoMessage()    {}
+func (*PlatformTypeList) Descriptor() ([]byte, []int) {
+	return fileDescriptor_5d492a0187472a3b, []int{5}
+}
+
+func (m *PlatformTypeList) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_PlatformTypeList.Unmarshal(m, b)
+}
+func (m *PlatformTypeList) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_PlatformTypeList.Marshal(b, m, deterministic)
+}
+func (m *PlatformTypeList) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_PlatformTypeList.Merge(m, src)
+}
+func (m *PlatformTypeList) XXX_Size() int {
+	return xxx_messageInfo_PlatformTypeList.Size(m)
+}
+func (m *PlatformTypeList) XXX_DiscardUnknown() {
+	xxx_messageInfo_PlatformTypeList.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_PlatformTypeList proto.InternalMessageInfo
+
+func (m *PlatformTypeList) GetType() []*Type {
+	if m != nil {
+		return m.Type
+	}
+	return nil
+}
+
+//三方注册请求
+type PlatformRequest struct {
+	Type                 *Type    `protobuf:"bytes,1,opt,name=type,proto3" json:"type,omitempty"`
+	PlatformId           string   `protobuf:"bytes,2,opt,name=platform_id,json=platformId,proto3" json:"platform_id,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *PlatformRequest) Reset()         { *m = PlatformRequest{} }
+func (m *PlatformRequest) String() string { return proto.CompactTextString(m) }
+func (*PlatformRequest) ProtoMessage()    {}
+func (*PlatformRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_5d492a0187472a3b, []int{6}
+}
+
+func (m *PlatformRequest) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_PlatformRequest.Unmarshal(m, b)
+}
+func (m *PlatformRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_PlatformRequest.Marshal(b, m, deterministic)
+}
+func (m *PlatformRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_PlatformRequest.Merge(m, src)
+}
+func (m *PlatformRequest) XXX_Size() int {
+	return xxx_messageInfo_PlatformRequest.Size(m)
+}
+func (m *PlatformRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_PlatformRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_PlatformRequest proto.InternalMessageInfo
+
+func (m *PlatformRequest) GetType() *Type {
+	if m != nil {
+		return m.Type
+	}
+	return nil
+}
+
+func (m *PlatformRequest) GetPlatformId() string {
+	if m != nil {
+		return m.PlatformId
+	}
+	return ""
+}
+
 func init() {
 	proto.RegisterType((*UserBaseInfo)(nil), "account.UserBaseInfo")
 	proto.RegisterType((*UserPlatformInfo)(nil), "account.UserPlatformInfo")
 	proto.RegisterType((*UserInfo)(nil), "account.UserInfo")
 	proto.RegisterType((*EmailOrPhoneRequest)(nil), "account.EmailOrPhoneRequest")
+	proto.RegisterType((*Type)(nil), "account.Type")
+	proto.RegisterType((*PlatformTypeList)(nil), "account.PlatformTypeList")
+	proto.RegisterType((*PlatformRequest)(nil), "account.PlatformRequest")
 }
 
 func init() { proto.RegisterFile("proto/account/account.proto", fileDescriptor_5d492a0187472a3b) }
 
 var fileDescriptor_5d492a0187472a3b = []byte{
-	// 313 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x6c, 0x92, 0x4f, 0x4b, 0xfb, 0x30,
-	0x18, 0xc7, 0x7f, 0x6d, 0xf7, 0x73, 0xee, 0xd9, 0x18, 0x23, 0x2a, 0xd4, 0x3f, 0xc3, 0xd1, 0xd3,
-	0x4e, 0x13, 0xe6, 0xd1, 0xd3, 0x06, 0x22, 0x3d, 0x39, 0x02, 0x7a, 0xb4, 0x64, 0xeb, 0x53, 0x0d,
-	0xac, 0x4d, 0x4d, 0xd2, 0xc3, 0xde, 0x85, 0x2f, 0x59, 0x92, 0xa6, 0xae, 0x4a, 0x4f, 0xcd, 0xf3,
-	0x29, 0xfd, 0x7e, 0xf2, 0x7c, 0x29, 0x5c, 0x97, 0x52, 0x68, 0x71, 0xc7, 0x76, 0x3b, 0x51, 0x15,
-	0xba, 0x79, 0x2e, 0x2c, 0x25, 0x7d, 0x37, 0x46, 0x6f, 0x30, 0x7a, 0x51, 0x28, 0xd7, 0x4c, 0x61,
-	0x5c, 0x64, 0x82, 0x8c, 0xc1, 0xe7, 0x69, 0xe8, 0xcd, 0xbc, 0x79, 0x40, 0x7d, 0x9e, 0x92, 0x73,
-	0xf8, 0x8f, 0x39, 0xe3, 0xfb, 0xd0, 0x9f, 0x79, 0xf3, 0x01, 0xad, 0x07, 0x43, 0xcb, 0x0f, 0x51,
-	0x60, 0x18, 0xd4, 0xd4, 0x0e, 0x84, 0x40, 0xaf, 0x60, 0x39, 0x86, 0x3d, 0x0b, 0xed, 0x39, 0xca,
-	0x60, 0x62, 0xf2, 0x37, 0x7b, 0xa6, 0x33, 0x21, 0x73, 0xeb, 0x20, 0xd0, 0xd3, 0x87, 0x12, 0x9d,
-	0xc5, 0x9e, 0xc9, 0x14, 0xc0, 0x5d, 0x29, 0xe1, 0xa9, 0x95, 0x05, 0x74, 0xe0, 0x48, 0x9c, 0x92,
-	0x5b, 0x18, 0x96, 0x2e, 0xc2, 0xbc, 0xaf, 0xb5, 0xd0, 0xa0, 0x38, 0x8d, 0xbe, 0x3c, 0x38, 0x35,
-	0x22, 0x2b, 0x78, 0x80, 0x71, 0xa5, 0x50, 0x26, 0x5b, 0xa6, 0x30, 0xe1, 0x45, 0x26, 0xac, 0x6a,
-	0xb8, 0xbc, 0x58, 0x34, 0x2d, 0xb4, 0x77, 0xa6, 0xa3, 0xaa, 0xdd, 0xc0, 0x13, 0x10, 0xfb, 0xf1,
-	0xd1, 0x67, 0x02, 0xfc, 0x59, 0x30, 0x1f, 0x2e, 0x2f, 0x7f, 0x05, 0xb4, 0x97, 0xa2, 0x93, 0xea,
-	0x0f, 0x89, 0x56, 0x70, 0xf6, 0x68, 0xda, 0x7a, 0x96, 0x1b, 0x53, 0x0f, 0xc5, 0xcf, 0x0a, 0x95,
-	0x3e, 0x36, 0xea, 0x75, 0x36, 0xea, 0xb7, 0x1a, 0x5d, 0x32, 0xe8, 0xaf, 0x6a, 0x21, 0x79, 0x85,
-	0x29, 0xc5, 0x77, 0xae, 0x34, 0x4a, 0x87, 0xd6, 0x87, 0x76, 0x3c, 0xb9, 0xf9, 0xb9, 0x5b, 0x87,
-	0xf5, 0xaa, 0x7b, 0xf5, 0xe8, 0xdf, 0xf6, 0xc4, 0xfe, 0x10, 0xf7, 0xdf, 0x01, 0x00, 0x00, 0xff,
-	0xff, 0x95, 0x4d, 0xbe, 0x18, 0x2f, 0x02, 0x00, 0x00,
+	// 428 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x74, 0x52, 0x51, 0xcf, 0xd2, 0x30,
+	0x14, 0x65, 0xdd, 0x14, 0xb9, 0x20, 0x92, 0xa2, 0x66, 0x80, 0x44, 0xe8, 0x13, 0x4f, 0x23, 0xc1,
+	0xf8, 0xe4, 0x13, 0x24, 0x84, 0x60, 0x4c, 0x24, 0x8d, 0xf8, 0x28, 0x19, 0xac, 0xc3, 0x25, 0x6c,
+	0x9d, 0x5b, 0xf7, 0xc0, 0xa3, 0xff, 0xc0, 0x9f, 0x6c, 0xda, 0xad, 0xb0, 0x6f, 0xec, 0x7b, 0x5a,
+	0x7b, 0xd6, 0x73, 0xcf, 0xb9, 0xe7, 0x5e, 0x18, 0xc5, 0x09, 0x17, 0x7c, 0xee, 0x9e, 0x4e, 0x3c,
+	0x8b, 0x84, 0xfe, 0x3a, 0x0a, 0xc5, 0xcd, 0xe2, 0x3a, 0x1c, 0x9d, 0x39, 0x3f, 0x5f, 0xd8, 0x5c,
+	0xc1, 0xc7, 0xcc, 0x9f, 0xaf, 0xc3, 0x58, 0x5c, 0xf3, 0x57, 0xe4, 0x17, 0x74, 0xf6, 0x29, 0x4b,
+	0x56, 0x6e, 0xca, 0xb6, 0x91, 0xcf, 0x71, 0x17, 0x50, 0xe0, 0xd9, 0xc6, 0xc4, 0x98, 0x99, 0x14,
+	0x05, 0x1e, 0x7e, 0x0b, 0x2f, 0x58, 0xe8, 0x06, 0x17, 0x1b, 0x4d, 0x8c, 0x59, 0x8b, 0xe6, 0x17,
+	0x89, 0xc6, 0xbf, 0x79, 0xc4, 0x6c, 0x33, 0x47, 0xd5, 0x05, 0x63, 0xb0, 0x22, 0x37, 0x64, 0xb6,
+	0xa5, 0x40, 0x75, 0x26, 0x3e, 0xf4, 0x64, 0xfd, 0xdd, 0xc5, 0x15, 0x3e, 0x4f, 0x42, 0xa5, 0x81,
+	0xc1, 0x12, 0xd7, 0x98, 0x15, 0x2a, 0xea, 0x8c, 0xc7, 0x00, 0x85, 0xdf, 0x43, 0xe0, 0x29, 0x31,
+	0x93, 0xb6, 0x0a, 0x64, 0xeb, 0xe1, 0x8f, 0xd0, 0x8e, 0x8b, 0x12, 0xf2, 0x7f, 0x2e, 0x0b, 0x1a,
+	0xda, 0x7a, 0xe4, 0x9f, 0x01, 0xaf, 0xa4, 0x90, 0x12, 0xf8, 0x02, 0xdd, 0x2c, 0x65, 0xc9, 0xe1,
+	0xe8, 0xa6, 0xec, 0x10, 0x44, 0x3e, 0x57, 0x52, 0xed, 0xc5, 0x3b, 0x47, 0x47, 0x54, 0xee, 0x99,
+	0x76, 0xb2, 0x72, 0x02, 0x1b, 0xc0, 0x8a, 0x7c, 0xd7, 0x93, 0x05, 0xd0, 0xc4, 0x9c, 0xb5, 0x17,
+	0x83, 0x27, 0x05, 0xca, 0x4d, 0xd1, 0x5e, 0x56, 0x41, 0xc8, 0x12, 0xfa, 0x6b, 0x99, 0xd6, 0xf7,
+	0x64, 0x27, 0xe3, 0xa1, 0xec, 0x4f, 0xc6, 0x52, 0x71, 0x4f, 0xd4, 0xa8, 0x4d, 0x14, 0x95, 0x12,
+	0x25, 0x0e, 0x58, 0x3f, 0x64, 0x3a, 0x75, 0x89, 0xe9, 0xb4, 0x51, 0x29, 0xed, 0xcf, 0xd0, 0xd3,
+	0x16, 0x24, 0xef, 0x5b, 0x90, 0x0a, 0x3c, 0xbd, 0x71, 0x65, 0x07, 0xaf, 0x6f, 0x1d, 0xc8, 0x07,
+	0x79, 0x29, 0xb2, 0x87, 0x37, 0x9a, 0xa6, 0x5d, 0x4e, 0x4b, 0x8a, 0xf5, 0xac, 0xea, 0x4c, 0x50,
+	0x75, 0x26, 0x8b, 0xbf, 0x08, 0x9a, 0xcb, 0x9c, 0x87, 0x7f, 0xc2, 0x98, 0xb2, 0x73, 0x90, 0x0a,
+	0x96, 0x14, 0xd0, 0xea, 0x5a, 0x4e, 0x07, 0x7f, 0xb8, 0x49, 0xd4, 0x84, 0x36, 0xac, 0x9f, 0x1c,
+	0x69, 0xe0, 0xaf, 0xd0, 0xdf, 0x30, 0xf1, 0xd0, 0xf4, 0x7b, 0x27, 0x5f, 0x7a, 0x47, 0x2f, 0xbd,
+	0xa3, 0x96, 0x7e, 0x78, 0x1f, 0x60, 0x95, 0x42, 0x1a, 0x98, 0xc2, 0xe0, 0xc1, 0xa3, 0x7e, 0x86,
+	0xed, 0x07, 0xa6, 0xf6, 0xf6, 0xfc, 0x52, 0x90, 0xc6, 0xf1, 0xa5, 0x32, 0xf0, 0xe9, 0x7f, 0x00,
+	0x00, 0x00, 0xff, 0xff, 0x5e, 0x41, 0x13, 0x03, 0xab, 0x03, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -294,6 +441,9 @@ const _ = grpc.SupportPackageIsVersion4
 type AccountClient interface {
 	//通过email或者手机号注册一个用户
 	RegisterAccountByEmailOrPhone(ctx context.Context, in *EmailOrPhoneRequest, opts ...grpc.CallOption) (*UserBaseInfo, error)
+	GetPlatformTypeList(ctx context.Context, in *empty.Empty, opts ...grpc.CallOption) (*PlatformTypeList, error)
+	//通过三方注册
+	RegisterAccountByPlatform(ctx context.Context, in *PlatformRequest, opts ...grpc.CallOption) (*UserPlatformInfo, error)
 }
 
 type accountClient struct {
@@ -313,10 +463,31 @@ func (c *accountClient) RegisterAccountByEmailOrPhone(ctx context.Context, in *E
 	return out, nil
 }
 
+func (c *accountClient) GetPlatformTypeList(ctx context.Context, in *empty.Empty, opts ...grpc.CallOption) (*PlatformTypeList, error) {
+	out := new(PlatformTypeList)
+	err := c.cc.Invoke(ctx, "/account.Account/GetPlatformTypeList", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *accountClient) RegisterAccountByPlatform(ctx context.Context, in *PlatformRequest, opts ...grpc.CallOption) (*UserPlatformInfo, error) {
+	out := new(UserPlatformInfo)
+	err := c.cc.Invoke(ctx, "/account.Account/RegisterAccountByPlatform", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // AccountServer is the server API for Account service.
 type AccountServer interface {
 	//通过email或者手机号注册一个用户
 	RegisterAccountByEmailOrPhone(context.Context, *EmailOrPhoneRequest) (*UserBaseInfo, error)
+	GetPlatformTypeList(context.Context, *empty.Empty) (*PlatformTypeList, error)
+	//通过三方注册
+	RegisterAccountByPlatform(context.Context, *PlatformRequest) (*UserPlatformInfo, error)
 }
 
 // UnimplementedAccountServer can be embedded to have forward compatible implementations.
@@ -325,6 +496,12 @@ type UnimplementedAccountServer struct {
 
 func (*UnimplementedAccountServer) RegisterAccountByEmailOrPhone(ctx context.Context, req *EmailOrPhoneRequest) (*UserBaseInfo, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method RegisterAccountByEmailOrPhone not implemented")
+}
+func (*UnimplementedAccountServer) GetPlatformTypeList(ctx context.Context, req *empty.Empty) (*PlatformTypeList, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetPlatformTypeList not implemented")
+}
+func (*UnimplementedAccountServer) RegisterAccountByPlatform(ctx context.Context, req *PlatformRequest) (*UserPlatformInfo, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method RegisterAccountByPlatform not implemented")
 }
 
 func RegisterAccountServer(s *grpc.Server, srv AccountServer) {
@@ -349,6 +526,42 @@ func _Account_RegisterAccountByEmailOrPhone_Handler(srv interface{}, ctx context
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Account_GetPlatformTypeList_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(empty.Empty)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(AccountServer).GetPlatformTypeList(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/account.Account/GetPlatformTypeList",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(AccountServer).GetPlatformTypeList(ctx, req.(*empty.Empty))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _Account_RegisterAccountByPlatform_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(PlatformRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(AccountServer).RegisterAccountByPlatform(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/account.Account/RegisterAccountByPlatform",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(AccountServer).RegisterAccountByPlatform(ctx, req.(*PlatformRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _Account_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "account.Account",
 	HandlerType: (*AccountServer)(nil),
@@ -356,6 +569,14 @@ var _Account_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "RegisterAccountByEmailOrPhone",
 			Handler:    _Account_RegisterAccountByEmailOrPhone_Handler,
+		},
+		{
+			MethodName: "GetPlatformTypeList",
+			Handler:    _Account_GetPlatformTypeList_Handler,
+		},
+		{
+			MethodName: "RegisterAccountByPlatform",
+			Handler:    _Account_RegisterAccountByPlatform_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
