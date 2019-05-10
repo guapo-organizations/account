b07c93317db87f32ebc7168fc11f78acc60af9cb
liangyu <liangyu@22.cn>
2019-05-10 18:47:21 +0800

    抛出异常

--- service/register.go
+++ service/register.go
@@ -12,16 +12,25 @@ func (this *AccountService) RegisterAccountByEmailOrPhone(ctx context.Context, i
 	register_service := lib.RegisterService{}
 
 	//邮箱注册
-	account_model, err := register_service.RegisterAccountByEmail(in.Email)
-	if err == nil {
+	if in.Email == "" {
+		account_model, err := register_service.RegisterAccountByEmail(in.Email)
+		//可能是格式出错
+		if err != nil {
+			return nil, err
+		}
 		return this.AccountModelChangeUserBaseInfo(account_model), nil
 	}
 
 	//手机号注册
-	account_model, err = register_service.RegisterAccountByPhone(in.Phone)
-	if err == nil {
+	if in.Phone == "" {
+		account_model, err := register_service.RegisterAccountByPhone(in.Phone)
+		//可能是格式出错
+		if err != nil {
+			return nil, err
+		}
+		
 		return this.AccountModelChangeUserBaseInfo(account_model), nil
 	}
 
-	return nil, fmt.Errorf("邮箱或者手机号注册失败")
+	return nil, fmt.Errorf("你想干嘛？你怎么不传参数？")
 }
