02902727ddccb1665ef2a7b827115a23e36c7bc4
liangyu <liangyu@22.cn>
2019-05-15 14:02:47 +0800

    grpc服务端添加tls

--- main.go
+++ main.go
@@ -6,7 +6,9 @@ import (
 	myservice "github.com/guapo-organizations/account-service/service"
 	"github.com/guapo-organizations/go-micro-secret/frame_tool"
 	grpc_service_info "github.com/guapo-organizations/go-micro-secret/frame_tool/service"
+	"github.com/guapo-organizations/go-micro-secret/tls"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"log"
 	"net"
 )
@@ -22,6 +24,12 @@ func main() {
 	my_frame_tool.Run()
 	service_info := grpc_service_info.GetGrpcServiceInfo()
 
+	//tls配置
+	//第一个参数是公钥，第二个参数是私钥
+	creds, err := credentials.NewServerTLSFromFile(tls.Path("server1.pem"), tls.Path("server1.key"))
+	if err != nil {
+		log.Fatalln("tls配置失败:", err)
+	}
 	//开始监听
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", service_info.Port))
 	if err != nil {
@@ -30,8 +38,8 @@ func main() {
 
 	log.Println(fmt.Sprintf("服务端口:%s", service_info.Port))
 
-	//构建服务
-	s := grpc.NewServer()
+	//构建服务,并启动tls配置
+	s := grpc.NewServer(grpc.Creds(creds))
 	//注册服务
 	pb.RegisterAccountServer(s, &myservice.AccountService{})
 
