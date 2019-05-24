67670817e1e515b7ac05a742a1ff80dbb70ead19
liangyu <liangyu@22.cn>
2019-05-24 21:19:42 +0800

    添加单元测试

--- service/service.go
+++ service/service.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"fmt"
 	account_service "github.com/guapo-organizations/account-service/lib/account"
 	"github.com/guapo-organizations/account-service/proto/account"
 	"github.com/guapo-organizations/go-micro-secret/consul"
@@ -21,13 +22,30 @@ func (this *AccountService) RegisterAccountByEmail(ctx context.Context, in *acco
 		return nil, err
 	}
 
+	//调用sms服务，查看验证吗是否正确
 	conn, err := my_grpc_connet.GetGrpcConnet("zldz.sms", "sms", consul_config)
 
 	if err != nil {
 		return nil, err
 	}
 
-	sms_service.NewSmsClient(conn)
+	sms_client := sms_service.NewSmsClient(conn)
+
+	sms_response, err := sms_client.ValidateCode(ctx, &sms_service.ValidateCodeRequest{
+		Key:         in.Email,
+		Code:        in.Code,
+		PublishMode: "email",
+		PublishType: "code",
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	//验证码不正确
+	if !sms_response.Result {
+		return nil, fmt.Errorf("验证码不正确")
+	}
 
 	//插入数据库
 	_, err = account_service.RegisterAccountByEmail(in.Email, in.Passwd)
---
+++ test/account_test.go
@@ -0,0 +1,39 @@
+package test
+
+import (
+	"context"
+	"fmt"
+	"github.com/guapo-organizations/account-service/proto/account"
+	"github.com/guapo-organizations/go-micro-secret/tls"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"log"
+	"testing"
+	"time"
+)
+
+func TestRegisterAccountByEmail(t *testing.T) {
+	//tls 和 ssl加密
+	creds, err := credentials.NewClientTLSFromFile(tls.Path("ca.pem"), "zldz.com")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	conn, err := grpc.Dial(fmt.Sprintf("%s:%d", "localhost", "50051"), grpc.WithTransportCredentials(creds))
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	client := account.NewAccountClient(conn)
+	response, err := client.RegisterAccountByEmail(ctx, &account.RegisterAccountEmailRequester{
+		Email:  "51785816@qq.com",
+		Code:   "123456",
+		Passwd: "123456789",
+	})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println(response)
+}
