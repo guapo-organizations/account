b3c3b82b81707d6cc0324096565115f7907206f0
liangyu <liangyu@22.cn>
2019-05-28 16:47:18 +0800

    添加一下模板

--- model/account_platform.go
+++ model/account_platform.go
@@ -4,9 +4,8 @@ import "github.com/jinzhu/gorm"
 
 type AccountPlatform struct {
 	gorm.Model
-	Id         int64  `gorm:"PRIMARY_KEY;AUTO_INCREMENT;column:id"`
 	AccountId  int64  `gorm:"column:account_id"`
-	TypeId       int64  `gorm:"column:type_id"`
+	TypeId     int64  `gorm:"column:type_id"`
 	PlatformId string `gorm:"column:platform_id"`
 }
 
---
+++ model/platform_type.go
@@ -0,0 +1,15 @@
+package model
+
+import "github.com/jinzhu/gorm"
+
+type PlatformType struct {
+	gorm.Model
+	Type      int64  `gorm:"column:type"`
+	AppId     int64  `gorm:"column:app_id"`
+	AppSecret string `gorm:"column:app_secret"`
+}
+
+//表的名字
+func (a PlatformType) TableName() string {
+	return "platform_type"
+}
